dc65878b95960acd845ea2bc734b3c8a7a35f7ae
Chrislock1 <fredrikcch@msn.com>
2016-04-06 17:46:19 +0200

    fixed minor details

--- Multiple-Elevator-Driver/Network/linkNetwork.py
+++ Multiple-Elevator-Driver/Network/linkNetwork.py
@@ -12,16 +12,16 @@ class LinkNode():
 	def __init__(self):
 
 		self.nodeNr = 0		#this is changed unless this is the first active node
-		self.IP = getMyIP
+		self.IP = getMyIP()
 		self.udpPort = 30000
 		self.tcpPort = 50001
 
-		self.tcpSocket = socket(AF_INET, SOCK_STREAM)
+		#self.tcpSocket = socket.socket(AF_INET, SOCK_STREAM)
 
-		self.broadcastSock = socket(AF_INET, SOCK_DGRAM)
+		self.broadcastSock = socket.socket(AF_INET, SOCK_DGRAM)
 		self.broadcastSock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-		self.udpRecvSock = socket(AF_INET, SOCK_DGRAM)
+		self.udpRecvSock = socket.socket(AF_INET, SOCK_DGRAM)
 		self.udpRecvSock.bind(("", self.udpPort))
 
 
@@ -34,20 +34,24 @@ class LinkNode():
 			self.broadcast("seekingConnections")
 			print "Broadcast done, waiting to recieve"
 
-			recvMsg, addr = self.udpRecvSock.recvfrom(1024) #socket is set up with timeout
+			recvMsg, addr = self.udpRecvSock.recvfrom(1024) #remember timeout
 
 			if(recvMsg == "seekingConnections"):
-				print "Recieved: {}\tFrom: {}".format(recvMsg, addr)
+				if(addr[0] == self.IP):
+					pass
+				else:
+					print "Recieved: {}\tFrom: {}".format(recvMsg, addr)
+
 				#forwardLinkWith(addr)	#either TCP connection, or UDP "connection"
 				#backwardLinkWith(addr)	
 				#return
-			elif(recvMsg == linkWithMe):
+			elif(recvMsg == "linkWithMe"):
 				pass
-			elif(recvMsg == alreadyLinked):
+			elif(recvMsg == "alreadyLinked"):
 				enterLinkNetwork()
 				return
 
-			time.sleep(0.1)
+			time.sleep(1)
 
 	def forwardLinkWith(self, addr):
 		#starts by sending link with me
